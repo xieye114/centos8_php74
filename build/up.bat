69a79e4d213141447728917ff5cf41812ac3506a
yyy <hatehate123@126.com>
2020-11-15 21:41:49 +0800

    fix

--- build/up.bat
+++ build/up.bat
@@ -9,4 +9,4 @@ if exist %path2%\mysql_data (
   md %path2%\mysql_data
 )
 
-docker  run  --name=cc  -tid -p 80:80 -v %mypath%/nginx_config:/etc/nginx/conf.d  -v %mypath%/code:/code -v %mypath%/composer/cache:/root/.cache/composer -v %mypath%/composer/config:/root/.config/composer -v %mypath%/root_config/history.txt:/root/.bash_history  -v %mypath%/root_config/bashrc.txt:/root/.bashrc  -v %mypath%/mysql_data:/var/lib/mysql --privileged=true  centos:8y   /usr/sbin/init
+docker  run  --name=cc  -tid -p 80:80 -p 3306:3306 -v %mypath%/nginx_config:/etc/nginx/conf.d  -v %mypath%/code:/code -v %mypath%/composer/cache:/root/.cache/composer -v %mypath%/composer/config:/root/.config/composer -v %mypath%/root_config/history.txt:/root/.bash_history  -v %mypath%/root_config/bashrc.txt:/root/.bashrc  -v %mypath%/mysql_data:/var/lib/mysql --privileged=true  centos:8y   /usr/sbin/init
